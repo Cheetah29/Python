296f6e6b468dc50cf1e67fbd6a66c72fe7155edb
Cheetah29 <68433121+Cheetah29@users.noreply.github.com>
2020-07-28 16:35:42 +0800

    Add files via upload

---
+++ pdf.py
@@ -0,0 +1,22 @@
+import glob
+import shutil
+
+if 'lol' == 'lol':
+    for f in glob.glob(r"C:\Users\admin\Downloads\*.pdf"):
+        shutil.move(f, r"C:\Users\admin\Downloads\PDFS")
+
+if 'lol' == 'lol':
+    for s in glob.glob(r"C:\Users\admin\Downloads\*.jpg"):
+        shutil.move(s, r"C:\Users\admin\Downloads\IMAGES")
+
+if 'lol' == 'lol':
+    for s in glob.glob(r"C:\Users\admin\Downloads\*.png"):
+        shutil.move(s, r"C:\Users\admin\Downloads\IMAGES")
+
+if 'lol' == 'lol':
+    for s in glob.glob(r"C:\Users\admin\Downloads\*.docx"):
+        shutil.move(s, r"C:\Users\admin\Downloads\WORD")
+
+if 'l' == 'l':
+    for s in glob.glob(r"C:\Users\admin\Downloads\*.pptx"):
+        shutil.move(s, r"C:\Users\admin\Downloads\PPT")
